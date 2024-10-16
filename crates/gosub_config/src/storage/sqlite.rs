6d8c0ed074348dd4aab3bec4092e55d99d09be24
Nicholas Nguyen <nicholasqnguyen8@gmail.com>
2024-10-15 20:14:42 -0400

    393: using ? instead of unwrap

--- crates/gosub_config/src/storage/sqlite.rs
+++ crates/gosub_config/src/storage/sqlite.rs
@@ -4,7 +4,7 @@ use gosub_shared::types::Result;
 use log::warn;
 use std::collections::HashMap;
 use std::str::FromStr;
-use std::sync::Mutex;
+use std::sync::{LockResult, Mutex};
 
 pub struct SqliteStorageAdapter {
     connection: Mutex<sqlite::Connection>,
@@ -31,11 +31,31 @@ impl TryFrom<&String> for SqliteStorageAdapter {
 
 impl StorageAdapter for SqliteStorageAdapter {
     fn get(&self, key: &str) -> Option<Setting> {
-        let db_lock = self.connection.lock().expect("Poisoned");
+        let db_lock = match self.connection.lock() {
+            Ok(l) => {l}
+            Err(e) => {
+                warn!("Poisoned mutex {e}");
+                return None
+            }
+        };
 
         let query = "SELECT * FROM settings WHERE key = :key";
-        let mut statement = db_lock.prepare(query).unwrap();
-        statement.bind((":key", key)).unwrap();
+        // If any of these sqlite commands fail at any point,
+        // Then we return a None
+        let mut statement = match db_lock.prepare(query) {
+            Ok(s) => {s}
+            Err(_) => {
+                warn!("problem preparing statement: {err}");
+                return None
+            }
+        };
+        match statement.bind((":key", key)) {
+            Ok(_) => {}
+            Err(_) => {
+                warn!("problem binding statement: {err}");
+                return None
+            }
+        };
 
         match Setting::from_str(key) {
             Ok(setting) => Some(setting),
@@ -46,27 +66,28 @@ impl StorageAdapter for SqliteStorageAdapter {
         }
     }
 
-    fn set(&self, key: &str, value: Setting) {
+    fn set(&self, key: &str, value: Setting) -> Result<()> {
         let db_lock = self.connection.lock().expect("Poisoned");
 
         let query = "INSERT OR REPLACE INTO settings (key, value) VALUES (:key, :value)";
-        let mut statement = db_lock.prepare(query).unwrap();
-        statement.bind((":key", key)).unwrap();
-        statement.bind((":value", value.to_string().as_str())).unwrap();
+        let mut statement = db_lock.prepare(query)?;
+        statement.bind((":key", key))?;
+        statement.bind((":value", value.to_string().as_str()))?;
 
-        statement.next().unwrap();
+        statement.next()?;
+        Ok(())
     }
 
     fn all(&self) -> Result<HashMap<String, Setting>> {
         let db_lock = self.connection.lock().expect("Poisoned");
 
         let query = "SELECT * FROM settings";
-        let mut statement = db_lock.prepare(query).unwrap();
+        let mut statement = db_lock.prepare(query)?;
 
         let mut settings = HashMap::new();
-        while let sqlite::State::Row = statement.next().unwrap() {
-            let key = statement.read::<String, _>(1).unwrap();
-            let value = statement.read::<String, _>(2).unwrap();
+        while let sqlite::State::Row = statement.next()? {
+            let key = statement.read::<String, _>(1)?;
+            let value = statement.read::<String, _>(2)?;
             settings.insert(key, Setting::from_str(&value)?);
         }
 
